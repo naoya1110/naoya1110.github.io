8086735164142053cfeb0ed527fbec54ad1da4ed
naoya1110 <naoya.1110@gmail.com>
2022-07-02 21:34:45 +0900

    add a link to googlemap

--- geolocation.js
+++ geolocation.js
@@ -6,11 +6,20 @@ function getGeoLocation(){
 function geoSuccess(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
+    const heading = Math.floor(position.coords.heading);
+    const speed = Math.floor(position.coords.speed);
     const accuracy = Math.floor(position.coords.accuracy);
     console.log("lat", lat)
     console.log("lng", lng)
-    document.getElementById("lat").innerHTML = lat;
-    document.getElementById("lng").innerHTML = lng;
+    document.getElementById("lat").innerHTML = "lat: " + lat;
+    document.getElementById("lng").innerHTML = "lng: " + lng;
+    document.getElementById("heading").innerHTML = "heading: " + heading;
+    document.getElementById("speed").innerHTML = "speed: " + speed;
+    document.getElementById("accuracy").innerHTML = "accuracy: "+accuracy;
+    url = "https://www.google.com/maps?q="+lat+"+"+lng
+    var maplink = document.getElementById("maplink")
+    maplink.href = url
+
 }
 
 function geoError(){
